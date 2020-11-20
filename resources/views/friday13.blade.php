142408497800c3d29ce1b8f0b07f14b07cd98125
Magdiel Juarez Guerrero <ganabbisativa@hotmail.com>
2020-11-20 14:15:39 -0600

    Add template to verify uri

---
+++ resources/views/friday13.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page Title</title>
+  </head>
+  <style media="screen">
+    /* Estilo de Caja 1 */
+    .caja1 {
+      background: green;
+      width: 30%;
+    }
+    .caja2 {
+      background: purple;
+      width: 30%;
+    }
+    .caja3 {
+      background: yellow;
+      width: 30%;
+    }
+    .position {
+      display: inline-block;
+    }
+  </style>
+
+  <style media="screen">
+    
+  </style>
+  <body>
+
+    <!-- Caja 1 -->
+    <div class="">
+      <header style="background: red;">
+        HEADER
+      </header>
+    </div>
+    <!-- Caja 2 -->
+    <div class="">
+      <div class="caja1 position">
+        CAJA 1
+      </div>
+      <div class="caja2 position">
+        CAJA 2
+      </div>
+      <div class="caja3 position">
+        CAJA 3
+      </div>
+    </div>
+
+  </body>
+</html>
