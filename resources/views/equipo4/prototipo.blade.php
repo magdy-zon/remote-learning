229dabeb0086eff4b929e13dd45deac9aafaa8bc
Alvaro <a_90_hernandez@hotmail.com>
2020-12-13 11:40:54 -0600

    Primeros avances

--- resources/views/equipo4/prototipo.blade.php
+++ resources/views/equipo4/prototipo.blade.php
@@ -2,7 +2,37 @@
 <html>
   <head>
     <title>Equipo 4: Prototipo</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('/equipo4/css/equipo4.css') }}"></link>
+  <link href="{!! url('/assets/materialize/css/materialize.css') !!}" rel="stylesheet">
+
   </head>
   <body>
+      <div class="header" id="header">
+        <div class="title" id="title">
+          <h1>What is Lorem Ipsum?</h1>
+        </div>
+      </div>
+
+      <div class="row" id="cards">
+          <div class="col s3 m3">
+            <div class="card grey-color small">
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card grey-color small">
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card grey-color small">
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card grey-color small">
+            </div>
+          </div>
+      </div>
   </body>
 </html>
