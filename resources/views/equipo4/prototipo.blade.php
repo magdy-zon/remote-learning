28652eb05895788067d2fcdd0a1b8fbc11fcfa8a
Alvaro <a_90_hernandez@hotmail.com>
2020-12-13 12:53:54 -0600

    Prototipo completo

--- resources/views/equipo4/prototipo.blade.php
+++ resources/views/equipo4/prototipo.blade.php
@@ -13,7 +13,7 @@
         </div>
       </div>
 
-      <div class="row" id="cards">
+      <div class="row" id="primary-cards">
           <div class="col s3 m3">
             <div class="card grey-color small">
             </div>
@@ -33,6 +33,35 @@
             <div class="card grey-color small">
             </div>
           </div>
+      </div>
+
+      <div class="row" id="secondary-cards">
+          <div class="col s6 m6 offset-s1 offset-m1">
+            <div class="card grey-color large">
+            </div>
+          </div>
+          <div class="col s4 m4">
+            <div class="card grey-color medium">
+            </div>
+            <div class="card grey-color medium">
+            </div>
+          </div>
+      </div>
+
+      <div class="row" id="content">
+        <div class="col s8 m8 offset-s2 offset-m2">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+      </div>
+
+      <div class="row" id="final-card">
+          <div class="col s8 m8 offset-s2 offset-m2">
+            <div class="card grey-color medium">
+            </div>
+          </div>
+      </div>
+
+      <div class="footer" id="footer">
       </div>
   </body>
 </html>
