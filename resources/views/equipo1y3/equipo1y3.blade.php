4dc20aa5bdca6ef1ade2351247d39c5b10ea82cc
Esmeralda Nava <ifglitterthen@gmail.com>
2020-12-13 21:30:56 -0600

    ENTREGA DE PRÁCTICA

---
+++ resources/views/equipo1y3/equipo1y3.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pantalla 1</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="{!! url('/equipo1y3/css/style.css') !!}" rel="stylesheet">
+</head>
+
+<body>
+    <!-- header -->
+    <div class="container">
+        <header class="w-center">
+            <div class="circle"></div>
+        </header>
+    </div>
+
+    <!-- 1st block, gray item -->
+    <section class="first_item">
+        <div class="gray_bg">
+            <div class="w-center">
+                <h1 class="main_ttl">What is Lorem Ipsum?</h1>
+                <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                    printing and typesetting industry.</p>
+                    <div class="d-flex w-center">
+                        <div class="cards_">
+                            <div class="square_gray"></div>
+                            <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                                printing and typesetting industry.</p>
+                        </div>
+                        <div class="cards_">
+                            <div class="square_gray"></div>
+                            <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                                printing and typesetting industry.</p>
+                        </div>
+                        <div class="cards_">
+                            <div class="square_gray"></div>
+                            <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                                printing and typesetting industry.</p>
+                        </div>
+                        
+                    </div>
+            </div>
+        </div>
+    </section>
+    <!-- 2nd block -->
+    <section class="w-center">
+        <h1 class="main_ttl_2">Where does it come from?</h1>
+        <div class="row_flex">
+            <div class="rectangle_gray">
+
+            </div>
+            <div class="rectangle_gray">
+
+            </div>
+        </div>
+    </section>
+
+    <!-- 3th block -->
+    <section class="w-center">
+        <div class="row_squares">
+            <div>
+                <div class="square_"></div>
+                <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                    printing and typesetting industry.</p>
+            </div>
+            <div>
+                <div class="square_"></div>
+                <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                    printing and typesetting industry.</p>
+            </div>
+            <div>
+                <div class="square_"></div>
+                <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                    printing and typesetting industry.</p>
+            </div>
+            <div>
+                <div class="square_"></div>
+                <p class="sub-ttl">Lorem Ipsum is simply dummy text of the <br>
+                    printing and typesetting industry.</p>
+            </div>
+            
+        </div>
+    </section>
+</body>
+
+</html>
